69f568ea27607b0332d4e2c4e967376212b2e37c
Sidx005 <desaisiddharth1246@gmail.com>
2024-09-19 20:26:55 +0530

    first commit

---
+++ Server/Server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+// const env=require('')
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use CORS middleware
+app.use(cors());
+
+// Read politicians data
+let politicians;
+try {
+  const rawData = fs.readFileSync(path.join(__dirname, 'api', 'data.json'), 'utf-8');
+  politicians = JSON.parse(rawData);
+  console.log(politicians);
+  
+} catch (error) {
+  console.error('Error reading or parsing data.json:', error);
+  politicians = []; // Provide a default empty array if file reading fails
+}
+
+// API endpoint
+app.get('/api/politicians', (req, res) => {
+  res.json(politicians);
+});
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
